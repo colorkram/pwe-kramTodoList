0ebf1c8a1580d5020ef59fbf8e83cd6e81b2caa2
colorkram <colorkram@gmail.com>
2023-12-26 17:36:31 +0700

    fix build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,38 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+const withPWA = require("@ducanh2912/next-pwa").default({
+    dest: "public",
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    disable: process.env.NODE_ENV === "development",
+    workboxOptions: {
+      disableDevLogs: true,
+    },
+    // ... other options you like
+  });
+  
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
+    output: 'standalone',
+    images: {
+      domains: [
+        "res.cloudinary.com",
+        "cdn.discordapp.com",
+        "lh3.googleusercontent.com",
+        "www.pcclean.io",
+        "192.168.1.88",
+        "192.168.1.40",
+        "localhost",
+        "*",
+        "api.travelmatch.biz",
+        "ttd-accounting.s3.ap-southeast-1.amazonaws.com",
+        "upload.wikimedia.org",
+        "s3-symbol-logo.tradingview.com"
+      ],
+    }}
+  
+  module.exports = withPWA(nextConfig);
